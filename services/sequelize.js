036ed5e72b21a659f5edad513a2a1070556f69dc
Ke Nguyen <ke.nguyen@2359media.com>
2018-01-22 17:51:16 +0700

    use cls to pass transactions to queries automaticlly

--- services/sequelize.js
+++ services/sequelize.js
@@ -1,12 +1,16 @@
+const CLS = require('continuation-local-storage');
+const namespace = CLS.createNamespace('Sequelize');
 const Sequelize = require('sequelize');
+
+Sequelize.useCLS(namespace);
 
 const sequelize = new Sequelize('lab_booking_transaction', 'root', '', {
   host: '127.0.0.1',
   dialect: 'mysql',
   logging: false,
   pool: {
-    min: 5,
-    max: 10
+    min: 2,
+    max: 5
   }
 });
 
